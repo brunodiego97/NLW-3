4861842837c36b7b1adbb00cb143a78ee3357229
Bruno Diego <brunodiego97@gmail.com>
2020-10-14 07:48:53 -0300

    First Day NLW

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+
+
+//isso abaixo é uma rota
+//Recurso = usuário
+
+/*
+Metodos HTTP = GET/POST/PUT/DELETE
+
+GET -> Buscar uma informação (Lista,item)
+POST -> Criar uma nova informação
+PUT -> Editar uma nova informação
+DELETE -> Deletar  uma nova informação
+*/
+
+/*Parametros
+
+Query params -> http://localhost:3333/users?serach=bruno (? =)
+
+Route params -> http://localhost:3333/users/1  (identificar um recurso PUT/DELETE)
+
+Body -> http://localhost:3
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import mapMakerImg from '../images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMakerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+
+          <strong>Minas Gerais</strong>
+          <span>Belo Horizonte</span>
+
+        </footer>
+      </aside>
+
+      <Map
+        center={[-19.9201985, -43.8768724]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.title.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
